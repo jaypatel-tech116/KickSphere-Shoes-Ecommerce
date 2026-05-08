7ad652e1fcc54f9342bceb307d4f146e3c691a9b
Jaypatel1126 <176357721+Jaypatel1126@users.noreply.github.com>
2026-05-08 09:14:09 +0530

    Fix SMTP timeout: Switch to Port 465 with SSL and Pooling

--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -5,10 +5,16 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  pool: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
